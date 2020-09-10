61bd52275f3896c269c0da65b236f3cea07c9580
nicolasxiao <54541638@qq.com>
2020-09-10 15:10:00 +0800

    feat: add template name testing

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,12 @@
-const { loadJson } = require("../lib/utils.js");
-const path = require('path')
+const { loadJson, makeGithubUrlForTbTemplate } = require("../lib/utils.js");
+const path = require("path");
 
 test("load json", () => {
-  const data = loadJson(path.resolve(__dirname, '../package.json'))
-  expect(data).not.toBeNull()
+  const data = loadJson(path.resolve(__dirname, "../package.json"));
+  expect(data).not.toBeNull();
+});
+
+test("generate github url by template name", () => {
+  const githubUrl = makeGithubUrlForTbTemplate("pcweb");
+  expect(githubUrl).toBe("https://github.com/tb-fed/template-pcweb.git");
 });
