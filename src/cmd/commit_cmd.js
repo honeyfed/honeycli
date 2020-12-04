1fdbb64c37440fca6f89fbc4a769781ad0011f69
nicolasxiao <54541638@qq.com>
2020-12-04 10:54:05 +0800

    feat: format code before commit

--- src/cmd/commit_cmd.js
+++ src/cmd/commit_cmd.js
@@ -1,90 +1,99 @@
-const inquirer = require('inquirer');
-const {  isGitRepo, getStatus, gitPush,getUnpushedCommits,gitAdd}  = require('../git/git');
-const path = require('path');
-const standardVersion = require('standard-version')
-const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
-const print = require('../util/print')
+const inquirer = require("inquirer");
+const {
+  getStatus,
+  gitPush,
+  getUnpushedCommits,
+  gitAdd,
+} = require("../git/git");
+const path = require("path");
+const standardVersion = require("standard-version");
+const bootstrap = require("commitizen/dist/cli/git-cz").bootstrap;
+const print = require("../util/print");
+const {formatCmd}=require('./format_cmd')
 
-function userCommit(){
-  return bootstrap({
-    cliPath: path.join(__dirname, '../node_modules/commitizen'),
-    // this is new
-    config: {
-      "path": path.resolve(__dirname, "../3rd/cz-conventional-changelog")
-    }
-  },'');
+function userCommit() {
+  return bootstrap(
+    {
+      cliPath: path.join(__dirname, "../node_modules/commitizen"),
+      // this is new
+      config: {
+        path: path.resolve(__dirname, "../3rd/cz-conventional-changelog"),
+      },
+    },
+    ""
+  );
 }
 
-
-async function gitCommit(){
+async function gitCommit() {
+  // format code
+  await formatCmd();
   // const git = await isGitRepo()
   // console.log('git', git)
-  const status = await getStatus()
-  const unPushed = await getUnpushedCommits()
-  if(unPushed){
+  const status = await getStatus();
+  const unPushed = await getUnpushedCommits();
+  if (unPushed) {
     console.log(
-      ` (${unPushed.split('\n').filter(u => u).length} commits unpushed)`
-    )
+      ` (${unPushed.split("\n").filter((u) => u).length} commits unpushed)`
+    );
   }
-  if(Array.isArray(status)) {
+  if (Array.isArray(status)) {
     const answer = await inquirer.prompt({
-      type: 'checkbox',
-      name: 'files',
-      message: '请选择要提交的文件:',
-      choices: status.map(n => {
-        return { name: n }
+      type: "checkbox",
+      name: "files",
+      message: "请选择要提交的文件:",
+      choices: status.map((n) => {
+        return { name: n };
       }),
-      pageSize: 20
-    })
+      pageSize: 20,
+    });
 
     if (answer && answer.files && answer.files.length > 0) {
       // add files
-      const filesToAdd = answer.files.length === status.length
-        ? '.'
-        : answer.files.join(' ')
-      const addRes = await gitAdd(filesToAdd)
-      if(addRes.code === 0){
-        await userCommit()
+      const filesToAdd =
+        answer.files.length === status.length ? "." : answer.files.join(" ");
+      const addRes = await gitAdd(filesToAdd);
+      if (addRes.code === 0) {
+        await userCommit();
       }
-       return true
+      return true;
     }
   }
 }
 
-async function gitPushCmd(){
+async function gitPushCmd() {
   // push
-  const unPushed = await getUnpushedCommits()
+  const unPushed = await getUnpushedCommits();
   if (unPushed) {
-
     const answer = await inquirer.prompt({
-      type: 'confirm',
-      name: 'pushCommits',
-      message: 'Do you want to push now?',
-      default: false
-    })
+      type: "confirm",
+      name: "pushCommits",
+      message: "Do you want to push now?",
+      default: false,
+    });
 
     if (answer.pushCommits) {
-      await gitPush('-u')
+      await gitPush("-u");
     }
   }
-
 }
 
-async function generateChangeLog(){
+async function generateChangeLog() {
   standardVersion({
     noVerify: true,
-    infile: 'docs/CHANGELOG.md',
-    silent: true
-  }).then(() => {
-    // standard-version is done
-    print.info('changelog 生成成功，位于docs/CHANGELOG.md文件中')
-  }).catch(err => {
-    print.error(`standard-version failed with message: ${err.message}`)
+    infile: "docs/CHANGELOG.md",
+    silent: true,
   })
+    .then(() => {
+      // standard-version is done
+      print.info("changelog 生成成功，位于docs/CHANGELOG.md文件中");
+    })
+    .catch((err) => {
+      print.error(`standard-version failed with message: ${err.message}`);
+    });
 }
 
 module.exports = {
   gitCommit,
   gitPushCmd,
-  generateChangeLog
-}
+  generateChangeLog,
+};
