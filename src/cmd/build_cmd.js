d7df8015767affb455ff46a28a9b18064be3c601
nicolasxiao <54541638@qq.com>
2020-12-09 15:09:42 +0800

    feat: build with cdn

--- src/cmd/build_cmd.js
+++ src/cmd/build_cmd.js
@@ -9,6 +9,35 @@ const { generateWebpackConfig } = require("../webpack/config");
 const { generateWebpackLibConfig } = require("../webpack/libconfig");
 const { loadHoneyConfig } = require("../util/config");
 const webpack = require("webpack");
+const { cdnCmd } = require("./cdn_cmd");
+
+function asyncWebpackBuild(webpackConfig) {
+  return new Promise((resolve, reject) => {
+    webpack(webpackConfig, (err, stats) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      process.stdout.write(
+        stats.toString({
+          colors: true,
+          modules: false,
+          children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+          chunks: false,
+          chunkModules: false,
+        }) + "\n\n"
+      );
+
+      if (stats.hasErrors()) {
+        print.error("  Build failed with errors.\n");
+        process.exit(1);
+      }
+
+      print.info("  Build complete.\n");
+      resolve();
+    });
+  });
+}
 
 async function buildCmd() {
   try {
@@ -42,25 +71,11 @@ async function buildCmd() {
       webpackConfig = generateWebpackConfig(config);
     }
 
-    webpack(webpackConfig, (err, stats) => {
-      if (err) throw err;
-      process.stdout.write(
-        stats.toString({
-          colors: true,
-          modules: false,
-          children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
-          chunks: false,
-          chunkModules: false,
-        }) + "\n\n"
-      );
+    await asyncWebpackBuild(webpackConfig);
 
-      if (stats.hasErrors()) {
-        print.error("  Build failed with errors.\n");
-        process.exit(1);
-      }
-
-      print.info("  Build complete.\n");
-    });
+    if (config.cdn) {
+      await cdnCmd();
+    }
   } catch (err) {
     print.error(err);
   }
