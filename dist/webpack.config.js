faa6825221774653a2800b2f9df67e093bfb08c9
nicolasxiao <54541638@qq.com>
2020-12-10 14:47:50 +0800

    feat: opt bin

---
+++ dist/webpack.config.js
@@ -0,0 +1,33 @@
+const nodeExternals =require('webpack-node-externals')
+const path= require('path')
+
+module.exports = {
+  target: 'node',
+  externals: [nodeExternals({
+    modulesDir: path.resolve(__dirname, '../node_modules')
+  })],
+  entry: '../src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+              'targets': {
+                'node': 'current'
+              }
+            }]
+          ]
+        }
+      }
+    }]
+  }
+}
