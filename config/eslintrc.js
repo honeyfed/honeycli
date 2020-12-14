4ac00706aa1e31c312019e97e9756dee3c239567
nicolasxiao <54541638@qq.com>
2020-12-14 15:13:07 +0800

    fix: vue eslint

--- config/eslintrc.js
+++ config/eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
   env: {
     browser: true,
   },
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaVersion: 2017,
+    sourceType: "module",
+  },
   extends: [
     "plugin:vue/essential",
     "plugin:react/recommended",
@@ -12,5 +17,5 @@ module.exports = {
     "prettier/react",
     "prettier/vue",
   ],
-  plugins: ["vue","react"],
+  plugins: ["vue", "react"],
 };
--- src/lint/lint.js
+++ src/lint/lint.js
@@ -6,6 +6,11 @@ function getEslintrc() {
     env: {
       browser: true,
     },
+    parserOptions: {
+      parser: "babel-eslint",
+      ecmaVersion: 2017,
+      sourceType: "module",
+    },
     extends: ["plugin:vue/essential","plugin:react/recommended","eslint-config-tencent"],
     plugins: ["vue","react"],
   };
